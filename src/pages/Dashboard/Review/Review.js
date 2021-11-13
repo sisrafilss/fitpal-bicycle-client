0f87bde620f6bf8bb2a1579d3f78ba458f648283
Israfil Hossen <sisrafilss@gmail.com>
2021-11-13 09:36:02 +0600

    review dynamically updated:

--- src/pages/Dashboard/Review/Review.js
+++ src/pages/Dashboard/Review/Review.js
@@ -2,18 +2,41 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import './Review.css'
 import ReactStars from "react-rating-stars-component";
+import { useState } from 'react/cjs/react.development';
+import useAuth from '../../../hooks/useAuth';
+import axios from 'axios';
 
 const Review = () => {
 
+    const { user } = useAuth();
+    const [rating, setRating] = useState(null);
+    const [reviewSubmited, setReviewSubmited] = useState(false);
+
     // Haqndle Rating
     const ratingChanged = (newRating) => {
-        console.log(newRating);
+        setRating(newRating);
     };
 
+
     // React hook form
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, reset, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => {
-        console.log(data);
+        const review = {
+            name: user.displayName,
+            img: user.photoURL,
+            rating: rating,
+            feedback: data.review
+        }
+
+        axios.post('http://localhost:5000/review', review)
+            .then(res => {
+                if (res.data.insertedId) {
+                    setReviewSubmited(true);
+                    reset();
+                };
+            })
+
+
     };
 
     return (
@@ -21,43 +44,58 @@ const Review = () => {
 
 
             <div className="login-form border rounded">
-                <div>
-                    <h2 className="h3 mb-4">Write a Review</h2>
-                </div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-
-                    <div className="mb-4">
-                        <ReactStars
-                            count={5}
-                            size={24}
-                            onChange={ratingChanged}
-                            isHalf={true}
-                            emptyIcon={<i className="far fa-star"></i>}
-                            halfIcon={<i className="fa fa-star-half-alt"></i>}
-                            fullIcon={<i className="fa fa-star"></i>}
-                            activeColor="rgb(250, 202, 81)"
-                        />
+                {
+                    reviewSubmited && <div>
+                        <div className="alert alert-success alert-dismissible fade show" role="alert">
+                            Review Submited.
+                            <button onClick={() => setReviewSubmited(false)} type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <div>
+                            <button onClick={() => setReviewSubmited(false)} className="btn btn-success">Write Another Review</button>
+                        </div>
                     </div>
+                }
+                {
+                    !reviewSubmited && <div>
+                        <div>
+                            <h2 className="h3 mb-4">Write a Review</h2>
+                        </div>
+                        <form onSubmit={handleSubmit(onSubmit)}>
 
-                    <div className="mb-3">
-                        <textarea
-                            rows="4"
-                            cols="50"
-                            type="text"
-                            className="form-control"
-                            {...register("review", { required: true })}
-                        />
-                        {errors.review && (
-                            <span className="text-danger">This field is required</span>
-                        )}
+                            <div className="mb-4">
+                                <ReactStars
+                                    count={5}
+                                    size={24}
+                                    onChange={ratingChanged}
+                                    isHalf={true}
+                                    emptyIcon={<i className="far fa-star"></i>}
+                                    halfIcon={<i className="fa fa-star-half-alt"></i>}
+                                    fullIcon={<i className="fa fa-star"></i>}
+                                    activeColor="rgb(250, 202, 81)"
+                                />
+                            </div>
+
+                            <div className="mb-3">
+                                <textarea
+                                    rows="4"
+                                    cols="50"
+                                    type="text"
+                                    className="form-control"
+                                    {...register("review", { required: true })}
+                                />
+                                {errors.review && (
+                                    <span className="text-danger">This field is required</span>
+                                )}
+                            </div>
+
+                            <input
+                                type="submit"
+                                className="btn btn-primary fw-bold"
+                                value="Submit"
+                            />
+                        </form>
                     </div>
-
-                    <input
-                        type="submit"
-                        className="btn btn-primary fw-bold"
-                        value="Submit"
-                    />
-                </form>
+                }
 
             </div>
 
