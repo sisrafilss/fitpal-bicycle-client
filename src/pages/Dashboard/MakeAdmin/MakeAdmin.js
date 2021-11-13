4380d9eba50cdb848475809e9166c283feda41a5
Israfil Hossen <sisrafilss@gmail.com>
2021-11-13 17:16:22 +0600

    make an admin functionality added

--- src/pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,55 +1,56 @@
+import axios from 'axios';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const MakeAdmin = () => {
 
-    // React Hook Form
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        reset,
-    } = useForm();
-    const onSubmit = (data) => {
-        console.log(data);
-        /* axios
-          .post(
-            "https://immense-journey-09745.herokuapp.com/add-a-new-service",
-            data
-          )
-          .then((res) => {
-            if (res.data?.insertedId) {
-              alert(
-                "Service added Successfully. Take a look at services page to check."
-              );
-              reset();
-            } else {
-              alert(res.data?.caused);
-              
-            }
-          }); */
-        reset();
-    };
+  // React Hook Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const onSubmit = (data) => {
+    axios
+      .put(
+        `http://localhost:5000/make-admin`,
+        data
+      )
+      .then((res) => {
+        if (res.data?.modifiedCount === 1) {
+          alert(
+            `${data.email} added to Admin List`
+          );
+          reset();
+        }
+        else if (res.data?.modifiedCount === 0) {
+          alert(
+            `${data?.email} already an Admin`
+          );
+        }
+      });
+  };
 
-    return (
-        <div className="form-container">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-3">
-                    <label className="form-label">Enter email address</label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Email"
-                        {...register("email", { required: true })}
-                    />
-                    {errors.email && (
-                        <span className="text-warning">This field is required</span>
-                    )}
-                </div>
-                <input type="submit" className="btn btn-primary" value="Submit" />
-            </form>
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label className="form-label">Enter email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+          {errors.email && (
+            <span className="text-warning">This field is required</span>
+          )}
         </div>
-    );
+        <input type="submit" className="btn btn-primary" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default MakeAdmin;
